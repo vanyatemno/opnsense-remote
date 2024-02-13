667703531dd9f08fe73d0b35693e8c4429217dc3
vanyatemno <vanya04400@gmail.com>
2024-02-13 14:51:34 +0200

    Update IndexController.php

--- mvc/app/controllers/OPNsense/HelloWorld2/IndexController.php
+++ mvc/app/controllers/OPNsense/HelloWorld2/IndexController.php
@@ -1,7 +1,8 @@
 <?php
 namespace OPNsense\HelloWorld2;
 
-class IndexController extends HelloWorld2 {
+class IndexController extends \OPNsense\Base\IndexController
+{
     public function indexAction () {
         // takes template (view) and give it to the user
         $this->view->pick('OPNsense/HelloWorld2/index')
