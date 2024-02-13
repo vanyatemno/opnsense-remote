c03d6bfa540784485b5e43658607d472fbf3992c
vanyatemno <vanya04400@gmail.com>
2024-02-13 14:45:49 +0200

    added initial files for HelloWorld2

---
+++ mvc/app/controllers/OPNsense/HelloWorld2/IndexController.php
@@ -0,0 +1,9 @@
+<?php
+namespace OPNsense\HelloWorld2
+
+class IndexController extends HelloWorld2 {
+    public function indexAction () {
+        // takes template (view) and give it to the user
+        $this->view->pick('OPNsense/HelloWorld2/index')
+    }
+}
---
+++ mvc/app/models/OPNsense/HelloWorld2/HelloWorld2.php
@@ -0,0 +1,8 @@
+<?php
+namespace OPNsense\HelloWorld2;
+
+use OPNsense\Base\BaseModel;
+
+class HelloWorld2 extends BaseModel {
+    
+}
